2cd8810e096a75ee4c1965bf0f563691a49643c8
banjoSolomon <ayomidebanjo68@gmail.com>
2024-08-05 15:40:49 +0100

    update

---
+++ src/Components/Navber/index.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return (
+        <div>
+            <h1>Welcome To Recycle Management System</h1>
+        </div>
+    )
+
+
+}
+export default Navbar
---
+++ src/routes/routes.index.jsx
@@ -0,0 +1,16 @@
+
+import Home from "../Pages/Home";
+import Layout from "../Components/Layout";
+
+export const ROUTES = [
+    {
+        path: '/',
+        element: <Layout/>,
+        children : [
+            {
+                path: '/',
+                element: <Home/>
+            },
+
+        ]
+    }]
