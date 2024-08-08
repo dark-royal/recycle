739e80a084c3a12cf1db0e38f8a4b8b271ee81a9
banjoSolomon <ayomidebanjo68@gmail.com>
2024-08-08 12:38:15 +0100

    update

--- src/Components/Navber/index.jsx
+++ src/Components/Navber/index.jsx
@@ -1,12 +1,24 @@
+import React, { useState } from 'react';
 import styles from './index.module.css';
 import logo1 from '../../asset/Vector (1).png';
 
 const Navbar = () => {
+    const [menuActive, setMenuActive] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuActive(!menuActive);
+    };
+
     return (
         <div className={styles.mainContainer}>
-            <div className={styles.sectionsWrapper}>
+            <div className={`${styles.sectionsWrapper} ${menuActive ? styles.menuActive : ''}`}>
                 <img src={logo1} alt="logo1" className={styles.logo} />
-                <h1 className={styles.title}>E-Treasure Trove</h1>
+                <h1 className={styles.title}>g-Cycle</h1>
+                <div className={styles.hamburger} onClick={toggleMenu}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
                 <div className={styles.section}>
                     <p>Home</p>
                     <p>Dashboard</p>
