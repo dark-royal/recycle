b6811472e1633b5a73fdaf4e9c50dc55dcf99449
banjoSolomon <ayomidebanjo68@gmail.com>
2024-08-12 11:33:25 +0100

    update

--- src/Pages/Clinet/Section2/section2.jsx
+++ src/Pages/Clinet/Section2/section2.jsx
@@ -1,29 +1,17 @@
 import styles from './index.module.css'
+import { keys } from './data'
+
 const Section2 = () => {
     return (
         <div className={styles.keysContainer}>
-            <div className={styles.key}>
-                <p className={styles.reduce}>Reduce</p>
-                <p>A key part of waste "reduction" is<br />
-                    "conservation"—using natural resources<br />
-                    wisely, and using less than usual in order<br />
-                    avoid waste.</p>
-            </div>
-            <div className={styles.key1}>
-                <p className={styles.highlight}>Reuse</p>
-                <p>You can "reuse" materials in their original<br />
-                    form instead of throwing them away, or pass<br />
-                    those materials on to others who could use them too<br />
-                    </p>
-            </div>
-            <div className={styles.key2}>
-                <p className={styles.recycle}>Recycle</p>
-                <p>Your recycling mission is not impossible! In<br />
-                    fact, it is very simple: Don't throw away<br />
-                    anything that can be recycled!</p>
-            </div>
+            {keys.map(({ id, title, description, className, textClassName }) => (
+                <div key={id} className={className}>
+                    <p className={textClassName}>{title}</p>
+                    <p>{description}</p>
+                </div>
+            ))}
         </div>
-    )
+    );
 }
 
 export default Section2;
