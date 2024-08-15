3c1498fe522fc62064f915aa8bf9364f0b059466
Elusoji Timileyin <elusojitimileyin@gmail.com>
2024-08-15 16:54:16 +0100

    working on blog page

---
+++ src/Components/blog/BlogPage.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import BlogCard from '../components/BlogCard';
+
+const blogPosts = [
+    {
+        title: 'Blog Post 1',
+        description: 'This is a short description of blog post 1.',
+        image: 'https://via.placeholder.com/600x400',
+    },
+    {
+        title: 'Blog Post 2',
+        description: 'This is a short description of blog post 2.',
+        image: 'https://via.placeholder.com/600x400',
+    },
+    {
+        title: 'Blog Post 3',
+        description: 'This is a short description of blog post 3.',
+        image: 'https://via.placeholder.com/600x400',
+    },
+];
+
+const Blog = () => {
+    return (
+        <div className="bg-gray-100 min-h-screen">
+            <div className="container mx-auto p-8">
+                <h1 className="text-4xl font-bold mb-8 text-center">My Blog</h1>
+                <div className="flex flex-wrap justify-center">
+                    {blogPosts.map((post, index) => (
+                        <BlogCard
+                            key={index}
+                            title={post.title}
+                            description={post.description}
+                            image={post.image}
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Blog;
