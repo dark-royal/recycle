20f5c0dd0824bfa2eccb9c9621043973dc50043e
michael <dikandumichael@gmail.com>
2024-09-03 00:54:46 +0100

    Updated the user dashboard

--- src/Components/dashboard/contentbar/Badge.jsx
+++ src/Components/dashboard/contentbar/Badge.jsx
@@ -4,7 +4,7 @@ const Badge = () => {
     return (
         <div className="flex-1 p-4 md:p-10 bg-customWhite w-full shadow-md flex flex-col items-baseline rounded-2xl">
             <p className="text-sm md:text-base">Badges Collected</p>
-            <div className="flex flex-wrap gap-2 md:gap-4 mt-2">
+            <div className="flex flex-row gap-2 md:gap-4 mt-2">
                 <img src={image} alt="badge" className="w-12 h-12 md:w-16 md:h-16" />
                 <img src={image} alt="badge" className="w-12 h-12 md:w-16 md:h-16" />
                 <img src={image} alt="badge" className="w-12 h-12 md:w-16 md:h-16" />
