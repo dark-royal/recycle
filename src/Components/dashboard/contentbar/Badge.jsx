c7f12eba56e03f71d18a37d01a5ae36923ea315f
michael <dikandumichael@gmail.com>
2024-09-03 00:53:13 +0100

    Updated the user dashboard

--- src/Components/dashboard/contentbar/Badge.jsx
+++ src/Components/dashboard/contentbar/Badge.jsx
@@ -1,15 +1,16 @@
 import image from "../../asset/badge1.png";
 
-const Badge = ()=>{
-    return(
-        <div className="flex-1 p-10 bg-customWhite w-100 shadow-md flex flex-col items-baseline rounded-2xl">
-            <p>Badges Collected</p>
-            <div className="flex">
-                <img src={image} alt="badge"/>
-                <img src={image} alt="badge"/>
-                <img src={image} alt="badge"/>
+const Badge = () => {
+    return (
+        <div className="flex-1 p-4 md:p-10 bg-customWhite w-full shadow-md flex flex-col items-baseline rounded-2xl">
+            <p className="text-sm md:text-base">Badges Collected</p>
+            <div className="flex flex-wrap gap-2 md:gap-4 mt-2">
+                <img src={image} alt="badge" className="w-12 h-12 md:w-16 md:h-16" />
+                <img src={image} alt="badge" className="w-12 h-12 md:w-16 md:h-16" />
+                <img src={image} alt="badge" className="w-12 h-12 md:w-16 md:h-16" />
             </div>
         </div>
-    )
-}
+    );
+};
+
 export default Badge;
